9b6a7e45e15d3e2af08ae171d1f496eefab7a878
Haysten Dcosta <haystendc@gmail.com>
2023-09-22 11:08:28 +0530

    KeyzNTonz website updated

---
+++ js/variables.js
@@ -0,0 +1,8 @@
+var a = "Haysten D'costa"; // not a good practice to use 'var'....
+console.log(a);
+
+{ // block of code....
+    var a = "Some other name";
+    console.log(a);
+}
+console.log(a);
