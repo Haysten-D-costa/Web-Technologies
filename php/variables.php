574f146facdf050afd274a8516c20ff6ced5fd75
Haysten D'costa <123768129+Haysten-D-costa@users.noreply.github.com>
2023-11-01 20:58:50 +0530

    Create variables.php

---
+++ php/variables.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Variables in PHP</title>
+</head>
+<body>
+    <?php
+        $a = 56; // integer
+        $b = 105.5; // float
+        $c = "Haysten D'costa"; // string
+        
+        echo "<br> Displaying values : ";
+        echo "<br> a = ".$a;
+        echo "<br> b = ".$b;
+        echo "<br> c = ".$c;
+    ?>
+<body>
+</html>
