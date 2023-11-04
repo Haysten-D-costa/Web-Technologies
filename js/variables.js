84d5f4969645c00664a380997d4c535fcd955062
Haysten Dcosta <haystendc@gmail.com>
2023-11-04 21:34:58 +0530

    Deleted php Folder

--- js/variables.js
+++
@@ -1,8 +0,0 @@
-var a = "Haysten D'costa"; // not a good practice to use 'var'....
-console.log(a);
-
-{ // block of code....
-    var a = "Some other name";
-    console.log(a);
-}
-console.log(a);
